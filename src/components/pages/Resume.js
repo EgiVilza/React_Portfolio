15c6eab635f952deaa88cf2d911ca6bfb8433990
EgiVilza <egivilza@gmail.com>
2021-05-11 11:38:47 -0500

    Updated Resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,13 +2,17 @@ import React from "react"
 import "../scss/style.css"
 import resume_1 from "../Images/resume_1.jpg"
 import resume_2 from "../Images/resume_2.jpg"
+import resume from "../Images/Resume_Portfolio.pdf"
 
 function Resume() {
     return (
         <div>
             {/* <p className="note">To Be Updated</p> */}
-            <img className="resume" src={resume_1}/>
-            <img className="resume" src={resume_2}/>
+            {/* <object className="resume" data={resume + "#view=FitH"} type="application/pdf">
+            </object> */}
+            <embed className="resume" src={resume + "#view=FitH"}></embed>
+            {/* <img className="resume" src={resume_1}/>
+            <img className="resume" src={resume_2}/> */}
         </div>    
     )
 }
