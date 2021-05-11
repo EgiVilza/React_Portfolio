435a84308120d4edb8dfaa4c3bc656af839ad6b5
EgiVilza <egivilza@gmail.com>
2021-05-11 10:56:05 -0500

    Fixed Resume Bug

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,14 @@
 import React from "react"
 import "../scss/style.css"
-import resume from "../Images/resume.pdf"
+import resume_1 from "../Images/resume_1.jpg"
+import resume_2 from "../Images/resume_2.jpg"
 
 function Resume() {
     return (
         <div>
             {/* <p className="note">To Be Updated</p> */}
-            {/* <object className="resume" data={resume + "#view=FitH"} type="application/pdf">
-            </object> */}
-            <embed className="resume" src={resume + "#view=FitH"}></embed>
+            <img className="resume" src={resume_1 + "#view=FitH"}/>
+            <img className="resume" src={resume_2}/>
         </div>    
     )
 }
